e5359d22c900cdfc3b11394f7160c82fe8371c79
isa-naranjo <cnscisabellanaranjo@gmail.com>
2024-02-07 10:24:48 -0500

    location2

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import './Location.css';
+import location from "../../acses/location.png";
 
 
 export  function Location () {
     return(
-        <h1>hola</h1>
+        <div className="location-container">
+            <div className="img-container">
+                <img src={location} alt="location" />
+            </div>
+            <div className="text-container">
+                <h4>Selecciona el método de entrega de tu pedido.</h4>
+            </div>
+
+        </div>
 
     );
 }
