451a3b5b8a04cb9f2f0ef80c28c8dca320fc9d7b
isa-naranjo <cnscisabellanaranjo@gmail.com>
2024-02-07 19:43:41 -0500

    NavBar_mobile

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,10 +4,17 @@ import discount from "../../acses/banner.png";
 
 export  function Banner () {
     return(
-        
+
+        <div className="Banner-container">
             <div className="discount">
                 <img src={discount} alt="discount" />
             </div>
+            <div class="button-container">
+                <button class="arrow-button"></button>
+                <button class="arrow-button"></button>
+            </div>
+
+        </div>
         
     );
 }
